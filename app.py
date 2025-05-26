7d7164efa9afd916f3892bc631a6700949246af4
alientx <38645713+alientx@users.noreply.github.com>
2025-05-26 14:02:17 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app=Flask(__name__) # __name__代表目前執行的模組
 def home():
     return "Hello Flask"
 
-# @app.route("/test") # 加一網站根目錄路徑
-# def test():
-#     return "This is a Test" 
+@app.route("/test") # 加一網站根目錄路徑
+def test():
+    return "This is a Test" 
 
 if __name__=="__main__": # 如果以主程式執行
     app.run() # 立刻啟動伺服器
